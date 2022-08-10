072d338724ee315b080cceb95fad9876b6b0f16d
Zakarias <46316857+zach721@users.noreply.github.com>
2022-08-11 00:19:09 +0200

    Add files via upload

---
+++ geek-to-latin.js
@@ -0,0 +1,138 @@
+function convert(){
+
+    
+    let text = 'γduckduckαααβγ'
+  
+    let search = 'α'
+    let replaceWith = 'a'
+    let search2 = 'β'
+    let replaceWith2 = 'b'
+    let search3 = 'γ'
+    let replaceWith3 = 'g'
+    let search4 = 'δ'
+    let replaceWith4 = 'd'
+    let search5 = 'ε'
+    let replaceWith5 = 'e'
+    let search6 = 'ζ'
+    let replaceWith6 = 'z'
+    let search7 = 'η'
+    let replaceWith7 = 'e'
+    let search8 = 'θ'
+    let replaceWith8 = 'th'
+    let search9 = 'ι'
+    let replaceWith9 = 'i'
+    let search10 = 'κ'
+    let replaceWith10 = 'k'
+    let search11 = 'λ'
+    let replaceWith11 = 'l'
+    let search12 = 'μ'
+    let replaceWith12 = 'm'
+    let search13 = 'ν'
+    let replaceWith13 = 'n'
+    let search14 = 'ξ'
+    let replaceWith14 = 'x'
+    let search15 = 'ο'
+    let replaceWith15 = 'o'
+    let search16 = 'π'
+    let replaceWith16 = 'p'
+    let search17 = 'ρ'
+    let replaceWith17 = 'r'
+    let search18 = 'ς'
+    let replaceWith18 = 's'
+    let search19 = 'σ'
+    let replaceWith19 = 's'
+    let search20 = 'τ'
+    let replaceWith20 = 't'
+    let search21 = 'υ'
+    let replaceWith21 = 'y'
+    let search22 = 'φ'
+    let replaceWith22 = 'ph'
+    let search23 = 'χ'
+    let replaceWith23 = 'ch'
+    let search24 = 'ψ'
+    let replaceWith24 = 'ps'
+    let search25 = 'ω'
+    let replaceWith25 = '0'
+    
+      
+      if(text.includes('α')){
+        text = text.split(search).join(replaceWith)
+      }
+      if(text.includes('β')){
+        text = text.split(search2).join(replaceWith2)
+      }
+      if(text.includes('γ')){
+        text = text.split(search3).join(replaceWith3)
+      }
+      if(text.includes('δ')){
+        text = text.split(search4).join(replaceWith4)
+      }
+      if(text.includes('ε')){
+        text = text.split(search5).join(replaceWith5)
+      }
+      if(text.includes('ζ')){
+        text = text.split(search6).join(replaceWith6)
+      }
+      if(text.includes('η')){
+        text = text.split(search7).join(replaceWith7)
+      }
+      if(text.includes('θ')){
+        text = text.split(search8).join(replaceWith8)
+      }
+      if(text.includes('ι')){
+        text = text.split(search9).join(replaceWith9)
+      }
+      if(text.includes('κ')){
+        text = text.split(search10).join(replaceWith10)
+      }
+      if(text.includes('λ')){
+        text = text.split(search11).join(replaceWith11)
+      }
+      if(text.includes('μ')){
+        text = text.split(search12).join(replaceWith12)
+      }
+      if(text.includes('ν')){
+        text = text.split(search13).join(replaceWith13)
+      }
+      if(text.includes('ξ')){
+        text = text.split(search14).join(replaceWith14)
+      }
+      if(text.includes('ο')){
+        text = text.split(search15).join(replaceWith15)
+      }
+      if(text.includes('π')){
+        text = text.split(search16).join(replaceWith16)
+      }
+      if(text.includes('ρ')){
+        text = text.split(search17).join(replaceWith17)
+      }
+      if(text.includes('ς')){
+        text = text.split(search18).join(replaceWith18)
+      }
+      if(text.includes('σ')){
+        text = text.split(search19).join(replaceWith19)
+      }
+      if(text.includes('τ')){
+        text = text.split(search20).join(replaceWith20)
+      }
+      if(text.includes('υ')){
+        text = text.split(search21).join(replaceWith21)
+      }
+      if(text.includes('φ')){
+        text = text.split(search22).join(replaceWith22)
+      }
+      if(text.includes('χ')){
+        text = text.split(search23).join(replaceWith23)
+      }
+      if(text.includes('ψ')){
+        text = text.split(search24).join(replaceWith24)
+      }
+      if(text.includes('ω')){
+        text = text.split(search25).join(replaceWith25)
+      }
+      
+      
+       console.log(text)
+    }
+    convert()
+    
